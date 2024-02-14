a61c9659fb301e5fc8364ad758943c3c768bf2f1
Phiraphat-Codediva <phiraphat@codediva.co.th>
2024-02-14 21:19:50 +0700

    update

--- src/endpoint/uploadfile/uploadfile.module.ts
+++ src/endpoint/uploadfile/uploadfile.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UploadfileService } from './uploadfile.service';
 import { UploadfileController } from './uploadfile.controller';
-import IBMServices from 'src/_helper/ibm-cloud';
 import { IBMModules } from 'src/_helper/imb-cloud.module';
 
 @Module({
@@ -11,7 +10,6 @@ import { IBMModules } from 'src/_helper/imb-cloud.module';
   controllers: [UploadfileController],
   providers: [
     UploadfileService,
-    IBMServices,
   ],
 })
 export class UploadfileModule {}
